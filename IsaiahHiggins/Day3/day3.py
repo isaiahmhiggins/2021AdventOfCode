12de745d1eab84ee0b44f24d9806e9a75dec95a6
Isaiah Higgins <isaiahmhiggins@cedarville.edu>
2021-12-02 23:06:11 -0700

    Day 3

--- IsaiahHiggins/Day3/day3.py
+++ IsaiahHiggins/Day3/day3.py
@@ -4,10 +4,37 @@ import Common as com
 
 #content = com.readAndCleanLine('Real.txt')
 content = com.readAndCleanLine('sample.txt')
-answer =0
 
-# file content
+def filterResults(pos, list, filter):
+    result = []
+    for line in list:
+        if line[pos] == filter:
+            result += [line]
+    return result
+
+def findLevel(data, control = None):
+    for row in range(0, len(data[1])):
+        col = []
+        if len(data) > 1:
+            for line in data:
+                col += line[row]
+            if len(data) > 1:
+                if control == 'oxygen':
+                    if(col.count('1') >= col.count('0')):
+                        data = filterResults(row, data, '1')
+                    else:
+                        data = filterResults(row, data, '0')
+                elif control == 'co2':
+                    if(col.count('1') < col.count('0')):
+                        data = filterResults(row, data, '1')
+        else:
+            break        
+    return int(data[0], 2)
 
 
-    
-print(answer)
+oxygen = findLevel(content, 'oxygen')
+co2 = findLevel(content, 'co2')
+print(oxygen)
+print(co2)
+print(co2*oxygen)
+
---
+++ IsaiahHiggins/Day4/day4.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(1, 'C:/Users/Isaiah Higgins/Documents/GitHub/2021AdventOfCode/IsaiahHiggins')
+import Common as com
+
+#content = com.readAndCleanLine('Real.txt')
+content = com.readAndCleanLine('sample.txt')
+answer =0
+
+# file content
+
+
+    
+print(answer)
