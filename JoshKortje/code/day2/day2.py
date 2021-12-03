aecdcd6ca52bb123b11da352a78fa6fb30e167e5
Joshua Kortje <joshuakortje@cedarville.edu>
2021-12-02 20:38:18 -0500

    Day 2 solved

--- JoshKortje/code/day2/day2.py
+++ JoshKortje/code/day2/day2.py
@@ -13,12 +13,29 @@ from collections import defaultdict
 file = open("input.txt", "r")
 
 # Split on every space
+depth = 0
+dist = 0
+aim = 0
 lines = list()
 for line in file:
-    lines.append(int(line.strip()))
+    instr, num = line.strip().split()
+    lines.append((instr, (int(num))))
 
+for text in lines:
+    word, change = text[0], text[1]
+    if word == 'forward':
+        dist += change
+        depth += aim*change
+    elif word == 'up':
+        aim -= change
+    elif word == 'down':
+        aim += change
+
+print(depth)
+print(dist)
 
 # print results
 print("Day 2")
+print(depth * dist)
 
 file.close()
