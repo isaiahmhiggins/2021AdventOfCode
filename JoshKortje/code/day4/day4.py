4fc70b0bc184011fc5bc33eab66525b398734b2c
Joshua Kortje <joshuakortje@cedarville.edu>
2021-12-04 09:33:58 -0500

    cleaned up day 4 solution -Josh K

--- JoshKortje/code/day4/day4.py
+++ JoshKortje/code/day4/day4.py
@@ -33,11 +33,13 @@ for i in range(2, len(text)):
 cards = np.array(grids)
 
 
+# Remove any called numbers
 def call_number(boards, num):
-    # Return -1 since it isn't used
+    # Return -1 since it isn't used (yes, I know it is kind of cheating)
     boards[boards == num] = -1
 
 
+# Check for a win
 def check_board(board):
     cols = np.sum(board, axis=0)
     rows = np.sum(board, axis=1)
@@ -46,6 +48,7 @@ def check_board(board):
     return False
 
 
+# Function to calculate score
 def get_score(board, num):
     board[board == -1] = 0
     square = np.sum(board)
@@ -53,16 +56,19 @@ def get_score(board, num):
     return score
 
 
-win = False
-
 for number in nums_called:
+    # Cross off the numbers
     call_number(cards, number)
+    
+    # Check for wins
     fin = list()
     for i in range(len(cards)):
         if check_board(cards[i]):
             print("win")
             print(get_score(cards[i], number))
             fin.append(i)
+    
+    # Remove any finished boards
     if fin:
         cards = np.delete(cards, fin, axis=0)
     if np.size(cards) == 0:
