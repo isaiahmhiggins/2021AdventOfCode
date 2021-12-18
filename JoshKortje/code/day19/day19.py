d0c412f254c008355e6af45e10ecca134d778676
Joshua Kortje <joshuakortje@cedarville.edu>
2021-12-18 16:03:03 -0500

    Solved day 18 -Joshua Kortje

---
+++ JoshKortje/code/day19/day19.py
@@ -0,0 +1,35 @@
+import numpy as np
+import copy
+import itertools
+import networkx as nx
+import string
+import re
+import math
+import heapq
+import binascii
+import binarytree as bt
+from scipy import ndimage
+from functools import lru_cache
+from collections import defaultdict
+from collections import Counter
+from collections import deque
+
+
+
+
+# Start of script
+file = open("input.txt", "r")
+
+# Parse the file
+text = list()
+for line in file:
+    text.append(line.strip())
+
+
+
+
+# print results
+print("Day 19")
+print()
+
+file.close()
